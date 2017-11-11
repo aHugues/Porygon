351809ce33e619b57d5cf68f8c12e25712fcd8bf
Aurélien Hugues <aurelien.huhues.59@gmail.com>
2017-11-11 20:56:24 +0100

    Improve movies list component look Add working search field

--- frontend/src/app/porygon/components/movies/list-movies.component.ts
+++ frontend/src/app/porygon/components/movies/list-movies.component.ts
@@ -17,31 +17,44 @@ export class ListMoviesComponent implements OnInit {
     pageSize = this.pageSizeOptions[0];
     currentPageIndex = 0;
     listLength = 0;
-    ready = false;
+    movieSearch = "";
 
     constructor(private porygonService: PorygonService) {}
 
     ngOnInit(): void {
         this.porygonService.getMoviesList()
             .subscribe(
-                (movies: Movie[]) => this.set_movies_list(movies),
+                (movies: Movie[]) => this.setMoviesList(movies),
                 error => console.error(error)
             );
     }
 
-    set_movies_list(movies: Movie[]) : void {
+    setMoviesList(movies: Movie[]) : void {
         this.listLength = movies.length;
         this.moviesList = movies;
-        let firstElement = this.currentPageIndex * this.pageSize;
-        let lastElement = firstElement + this.pageSize;
-        this.moviesListPage = movies.slice(firstElement, lastElement);
-        this.ready = true;
+        this.updateDisplayedList();
     }
 
-    onPageChange(event: any): void {
-        this.currentPageIndex = event.pageIndex;
+    updateDisplayedList(): void {
         let firstElement = this.currentPageIndex * this.pageSize;
         let lastElement = firstElement + this.pageSize;
         this.moviesListPage = this.moviesList.slice(firstElement, lastElement);
     }
+
+    onPageChange(event: any): void {
+        this.currentPageIndex = event.pageIndex;
+        this.updateDisplayedList();
+    }
+
+    searchMovies(): void {
+        let query = {title: this.movieSearch};
+        this.porygonService.getMovies(query)
+            .subscribe(
+                (movies: Movie[]) => {
+                    this.setMoviesList(movies);
+                },
+                (error) => console.error(error)
+            );
+    }
+    
 }
